143cde9aabbc031a8161d4b6e59f7e3afc7be031
root <tobijthowe@gmail.com>
2022-03-11 23:02:11 +0000

    Worked on gamemode

--- ClassicMode.cpp
+++ ClassicMode.cpp
@@ -15,7 +15,7 @@ int ClassicMode::ScanNeighbors(Grid grid){
             for(int x = i - 1; x <= i +1; x++){ //check row above and below
                 for(int y = j + 1; y <= j + 1; y++){ //check column above and below
                     if((x != 0) || (x != i) || (y != 0) || (y != j)){
-                        
+
                     }
                 }
             }
